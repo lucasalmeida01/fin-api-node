a6b0f4b4f6875f8449e26965888dc2d05cd8eb3c
lucasalmeida01 <lucas.almeida1705@outlook.com>
2022-02-14 10:58:20 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { v4: uuidv4 } = require("uuid")
+
+const app = new express();
+
+app.use(express.json());
+
+const customers = []
+
+/* Dados da conta
+*cpf - string
+*name - string
+*id - uuid - se trata de um universal unique id, um id gerado por uma biblioteca
+*statement - []
+*/
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
+  return response.status(201).send();
+});
+
+//localhost:8080
+app.listen('8080')
